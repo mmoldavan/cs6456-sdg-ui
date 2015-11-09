290e7a2d5f17738d02a670233cb8235abaadbabe
mmoldavan <matt.moldavan@careerbuilder.com>
2015-11-09 11:45:08 -0500

    add some splashes to the paddle stroke

--- Assets/SDG/OarControl.cs
+++ Assets/SDG/OarControl.cs
@@ -3,10 +3,16 @@ using System.Collections;
 
 public class OarControl : MonoBehaviour {
 	public BoatUserControl boatControl;
-	
+	private ParticleSystem splashParticleSystem;
+
+	public void Start ()
+	{
+		splashParticleSystem = this.transform.GetComponentInChildren<ParticleSystem> ();
+	}
+
 	public void CreateWaterSplash()
 	{
-		
+		splashParticleSystem.Play ();
 	}
 
 	public void leftOarFinished()
